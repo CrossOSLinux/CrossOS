dbbaea03fab6edb5226159b5670a396e6111fa7b
Vaibhav Kumar <vaibhavk2032@gmail.com>
2026-08-08 09:39:23 +0530

    Packages: Added hyprland support and replaced pacman commands in packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-# Cross OS - Package Installer
+# Cross OS - Package Installer 
 set -e
 
 REPO_DIR=~/CrossOS
+
+# Add Hyprland 
+sudo cp /usr/share/xbps.d/00-repository-main.conf /etc/xbps.d/
+sudo sed -i "1i repository=https://mirror.black-hole.dev/$(xbps-uhelper arch)" /etc/xbps.d/00-repository-main.conf
+
+# Add Noctalia Shell 
+# echo "repository=https://repo.voiders.dev" | sudo tee /etc/xbps.d/10-voiders-community.conf
+
+sudo xbps-install -S # sync packges with repos.
 
 install_packages() {
     local file=$1
     while IFS= read -r pkg || [[ -n "$pkg" ]]; do
         [[ -z "$pkg" || "$pkg" == \#* ]] && continue
-        if pacman -Q "$pkg" &>/dev/null; then
+        if xbps-query -S "$pkg" &>/dev/null; then
             echo "$pkg already installed, skipping..."
         else
             install_with_retry "$pkg"
@@ -19,7 +28,7 @@ install_packages() {
 install_with_retry() {
     local pkg=$1
     local attempts=0
-    until sudo xbps-install -S --noconfirm "$pkg"; do
+    until sudo xbps-install -Sy "$pkg"; do
         attempts=$((attempts + 1))
         if [ $attempts -eq 5 ]; then
             echo "Failed to install $pkg after 5 attempts, skipping..."
@@ -38,7 +47,8 @@ gum style \
     --width 50 \
     "Updating the system"
 
-sudo pacman -Syu --noconfirm
+# XBPS system update
+sudo xbps-install -Syu
 
 case "$1" in
     --dev)
