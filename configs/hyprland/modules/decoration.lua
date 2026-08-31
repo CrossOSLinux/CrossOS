2db66e7de32b71bd7f456b94e3fe2bbb0ea0c23c
Vaibhav Kumar <vaibhavk2032@gmail.com>
2026-08-31 22:19:50 +0530

    Hyprland Config Split: Decoration lua

---
+++ configs/hyprland/modules/decoration.lua
@@ -0,0 +1,22 @@
+-- Decoration section of Hyprland config
+hl.config({
+	decoration = {
+		rounding = 0,
+
+		active_opacity = 1.0,
+		inactive_opacity = 0.77,
+
+		shadow = {
+			enabled = true,
+			range = 4,
+			color = 0xee1a1a1a,
+		},
+
+		blur = {
+			enabled = true,
+			size = 3,
+			passes = 2,
+			vibrancy  = 0.1696,
+		},
+	} 
+})
