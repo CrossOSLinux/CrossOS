0b2cfef2e85d0b6ad1f27fd8c0a71910baa8e630
Vaibhav Kumar <vaibhavk2032@gmail.com>
2026-08-31 22:20:24 +0530

    Hyprland Config Split: Environment vars and General lua

---
+++ configs/hyprland/modules/general.lua
@@ -0,0 +1,15 @@
+-- Gneral section of Hyprland config
+
+hl.config({
+	general = {
+		gaps_in  = 5,
+		gaps_out = 10,
+
+		border_size = 1,
+
+		col = {
+			active_border = "rgba(cdd6f4ee)",
+			inactive_border = "rgba(595959aa)",
+		},		
+	},
+)}
