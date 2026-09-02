b5242eec31b92eedd853629b584890963f86dfdf
Vaibhav Kumar <vaibhavk2032@gmail.com>
2026-09-02 20:54:05 +0530

    Hyprland: Made Mac OS like top window bar.

--- configs/hyprland/plugins/hyprbars.lua
+++ configs/hyprland/plugins/hyprbars.lua
@@ -4,19 +4,26 @@ hl.on("hyprland.start", function ()
   	hl.exec_cmd("hyprpm enable hyprbars")
 end)
 
+
 hl.config({
     plugin = {
         hyprbars = {
-            bar_height = 20,
-            on_double_click = "hl.dsp.window.fullscreen({action = 'set'})",
+            bar_height = 50,
+			bar_color  = "rgb(363636)",
+			bar_text_size = 24,
+            
+            on_double_click = 'hl.dsp.window.fullscreen( mode = "fullscreen" ,{action = "set"})'', -- does not work
+
+            
         },
     },
 })
 
+
 hl.plugin.hyprbars.add_button({
     bg_color = "rgb(ff4040)",
     fg_color = "rgb(ffffff)",
-    size = 10,
+    size = 20,
     icon = "X",
     action = "hyprctl dispatch 'hl.dsp.window.close()'",
 })
@@ -24,7 +31,7 @@ hl.plugin.hyprbars.add_button({
 hl.plugin.hyprbars.add_button({
     bg_color = "rgb(eeee11)",
     fg_color = "rgb(000000)",
-    size = 10,
+    size = 20,
     icon = "_",
     action = [[hyprctl dispatch 'hl.dsp.window.fullscreen({ mode = "fullscreen", action = "toggle" })']],
 })
